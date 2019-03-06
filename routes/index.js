9e0f0152b443bd67bfd67d08574b5bce472d3026
honghanh3123 <hanhanhhueduy@gmail.com>
2019-03-06 16:06:01 +0700

    update webhook

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.status(200).send('oke')
 });
 
+router.get('/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === 'thoconyeucafrotos') {
+    res.send(req.query['hub.challenge']);
+  }
+  else res.send('Error, wrong validation token');
+});
+
 module.exports = router;
