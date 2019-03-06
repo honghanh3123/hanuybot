066f04a18529048d1093deadf4da90fc79c06ede
honghanh3123 <hanhanhhueduy@gmail.com>
2019-03-06 17:13:27 +0700

    callsendapi

--- routes/index.js
+++ routes/index.js
@@ -22,10 +22,36 @@ router.post("/webhook", (req, res)=>{
     messages.map(message =>{
       let sender = message.sender.id;
       let mess = message.message.text;
-      console.log(sender, mess)
+      callSendAPI(sender, mess)     
     }) 
   })
   res.status(200).send('ok')
 })
+
+function callSendAPI(sender_psid, message) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": {
+      "text": message
+    }
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": "EAALTPHvvB5sBAGofyA67aTA3YoTWjCblSg6hrB7FhYkQ7OFYgeVJsdAsMZCfoqZBzYHiXrP4nrYNdlwyRBmhqrjDAG3t7v2ZAmG4t0sFCO4ajYANToWD8wLLSBNiM1DpDvbAOZBzsyrXVJh0clkmP4wUckE2L4GTw0FkZBaFR8QZDZD" },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  });
+}
 
 module.exports = router;
